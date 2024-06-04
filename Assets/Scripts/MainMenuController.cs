1128ec27c52b905f13def467ee30da3b21ca14f6
Alberto Cimino <ciminoalberto6@gmail.com>
2024-06-04 22:12:34 +0200

    Main Menu basic

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public CanvasGroup OptionPanel;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void Option()
+    {
+        OptionPanel.alpha = 1;
+        OptionPanel.blocksRaycasts = true;
+    }
+    public void Back()
+    {
+        OptionPanel.alpha = 0;
+        OptionPanel.blocksRaycasts = false;
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
