9e571d55b7676f551e9672b618cd8d6d9a9fca68
Kurai Kunshu <gabrielesanzogni2@gmail.com>
2024-06-13 15:17:31 +0200

    Wave System ans Spawner (to test)

--- Assets/Scripts/Enemy/WaveManager.cs
+++ Assets/Scripts/Enemy/WaveManager.cs
@@ -2,13 +2,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
+using Unity.Properties;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Serialization;
 public enum WaveStage { Safe, EnemyAttack }
+
+[Serializable]
+public class DictionaryItem {
+    public PoolData Key;
+    public int Value;
+}
 public class WaveManager : MonoBehaviour, IPoolRequester {
 
     #region SerializedField
-    [SerializeField] private PoolData[] enemyTypes;
+    //[SerializeField] private PoolData[] enemyTypes;
+    [SerializeField][Tooltip("List of different type of enemy and the level they starts to spawn")] 
+    private DictionaryItem[] enemyTypes;
+    
     [SerializeField][Tooltip("Final Level to reach")] private int victoryLevel;
     [SerializeField] private bool isInfinite; 
     
@@ -19,7 +31,7 @@ public class WaveManager : MonoBehaviour, IPoolRequester {
     private float timeNoise;
     
     [SerializeField][Range(1, 10)] private float levelDifficultyGrowth;
-    [SerializeField] private int startingNOfEnemy;
+    [SerializeField][Tooltip("Number Of enemy at the first wave")] private int numberOfEnemy;
     #endregion
 
     #region privateAttribute
@@ -30,7 +42,16 @@ public class WaveManager : MonoBehaviour, IPoolRequester {
     #endregion
     
     #region Property
-    public PoolData[] Datas { get => enemyTypes; }
+    public PoolData[] Datas {
+        get {
+            PoolData[] poolData = new PoolData[enemyTypes.Length];
+            for (int i = 0; i < enemyTypes.Length; i++) {
+                poolData[i] = enemyTypes[i].Key;
+            }
+            
+            return poolData;
+        }
+    }
     #endregion
 
     #region Mono
@@ -38,7 +59,7 @@ public class WaveManager : MonoBehaviour, IPoolRequester {
         spawners = new Spawner[enemyTypes.Length];
         for (int i = 0; i < enemyTypes.Length; i++) {
             spawners[i] = gameObject.AddComponent<Spawner>();
-            spawners[i].Init(enemyTypes[i], timeNoise);
+            spawners[i].Init(enemyTypes[i].Key, timeNoise);
         }
 
         elapsedTime = 0;
@@ -76,14 +97,14 @@ public class WaveManager : MonoBehaviour, IPoolRequester {
         level++;
         float fuzzyDifficulty = level * levelDifficultyGrowth * 0.5f;
         
-        //decidi, in base alla difficolta, quale e quanti spawner prendere
-        
         //ogni x aumenta il numero di nemici da spawnare
-        startingNOfEnemy += (int)fuzzyDifficulty;
+        numberOfEnemy += (int)fuzzyDifficulty;
         
-        //StartSpawn()
-        if (level % levelDifficultyGrowth == 0) {
-            //ogni x livelli aumenti la difficolta
+        //decidi, in base alla difficolta, quale e quanti spawner prendere
+        for (int i = 0; i < enemyTypes.Length; i++) {
+            if (enemyTypes[i].Value >= fuzzyDifficulty) {
+                spawners[i].StartSpawn(numberOfEnemy, waveDuration, fuzzyDifficulty );
+            }
         }
     }
 
