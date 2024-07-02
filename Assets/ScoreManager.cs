334ac6c085468c9622b4efe247768d7b290b3b67
Alberto Cimino <ciminoalberto6@gmail.com>
2024-07-02 15:24:12 +0200

    ScoreManager

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+    public Text scoreText;
+    int score = 0;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        scoreText.text = "SCORE: " + score.ToString();
+    }
+
+    
+    public void AddPoint()
+    {
+        score += 1;
+        scoreText.text = "SCORE: " + score.ToString();
+    }
+}
+
